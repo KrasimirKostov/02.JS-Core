c5d919700385a43ff9a8be0d88f114af70263eb7
Krasimir Kostov <krasimir_kostov@dir.bg>
2019-07-02 23:32:45 +0300

    Add files via upload

---
+++ 2019.01.19_JS Fundamentals/06. EXERCISE DOCUMENT OBJECT MODEL/02_Chat-Room-with-Pesho/solution.js
@@ -0,0 +1,45 @@
+function solve() {
+
+    // let buttons = document.getElementsByTagName('button');
+
+    //0 is mine, 1 is pesho
+    let buttons = Array.from(document.getElementsByTagName('button'));//js masiv
+    let inputFields = Array.from(document.getElementsByTagName('input'));
+
+    buttons.forEach((btn) => {
+        btn.addEventListener('click', function (event) {
+            // console.log(event.target);
+            let divElement = document.createElement('div');
+            let spanElement = document.createElement('span');
+            let pElement = document.createElement('p');
+
+            let senderBtn = event.target.name;
+
+            if (senderBtn === 'myBtn') {
+                spanElement.textContent = 'Me';
+                pElement.textContent = document.getElementById('myChatBox').value;
+
+                divElement.style.textAlign='left';
+
+            } else if (senderBtn === 'peshoBtn') {
+                spanElement.textContent = 'Pesho';
+                pElement.textContent = document.getElementById('peshoChatBox').value;
+
+                divElement.style.textAlign='right';
+
+            }
+
+            divElement.appendChild(spanElement);
+            divElement.appendChild(pElement);
+
+           
+
+            document.getElementById('chatChronology').appendChild(divElement);
+
+            inputFields[0].value = '';
+            inputFields[1].value = '';
+        })
+    });
+
+
+}
