0b853eaf2936c719199c1e58bb5b1b7a361bcdd3
Krasimir Kostov <krasimir_kostov@dir.bg>
2019-07-03 00:14:39 +0300

    Add files via upload

---
+++ 2019.03.19_JS Applications/11. EXERCISE TEMPLATING/05. JS-Applications-Templating-Exercise-Resources/HTTP-Status-Cats/template.js
@@ -0,0 +1,21 @@
+$(() => {
+    renderCatTemplate();
+
+    async function renderCatTemplate() {
+        let catsTemplate = await $.ajax({
+            url: './catsTemplate.html'
+        })
+        let compiledTemplate = Handlebars.compile(catsTemplate);
+        let context = {
+            cats: window.cats
+        };
+        $('#allCats').html(compiledTemplate(context));
+
+    }
+
+})
+
+function showInfo(id) {
+    $(`#${id}`).toggle();
+
+}
